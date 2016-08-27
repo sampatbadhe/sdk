6d5f150b733c19b72ee8f1e46b6db05b4dc76d6d
Karan <karan.valecha92@gmail.com>
2016-08-27 18:46:50 +0530

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,26 @@ ActiveRecord::Schema.define(version: 20160827120204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "helpline_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "helplines", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.integer  "phone_number"
+    t.integer  "mobile_number"
+    t.integer  "helpline_category_id"
+    t.integer  "region_id"
+    t.string   "email_address"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.index ["helpline_category_id"], name: "index_helplines_on_helpline_category_id", using: :btree
+    t.index ["region_id"], name: "index_helplines_on_region_id", using: :btree
+  end
+
   create_table "members", force: :cascade do |t|
     t.integer  "phone_number"
     t.string   "first_name"
@@ -26,6 +46,12 @@ ActiveRecord::Schema.define(version: 20160827120204) do
     t.index ["society_id"], name: "index_members_on_society_id", using: :btree
   end
 
+  create_table "regions", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "societies", force: :cascade do |t|
     t.integer  "registration_number"
     t.string   "name"
@@ -35,5 +61,7 @@ ActiveRecord::Schema.define(version: 20160827120204) do
     t.datetime "updated_at",          null: false
   end
 
+  add_foreign_key "helplines", "helpline_categories"
+  add_foreign_key "helplines", "regions"
   add_foreign_key "members", "societies"
 end
