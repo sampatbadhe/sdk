c374ffdf302c1e3885af1771952643dcf8effdfd
sampatbadhe <sampat.badhe@kiprosh.com>
2016-08-27 23:05:08 +0530

    integrate devise for member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
 class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, authentication_keys: [:email]
   belongs_to :society
   has_many :topics
   has_many :comments
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827134537) do
+ActiveRecord::Schema.define(version: 20160827152643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,8 +52,20 @@ ActiveRecord::Schema.define(version: 20160827134537) do
     t.string   "last_name"
     t.integer  "society_id"
     t.boolean  "is_secretary"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_members_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
     t.index ["society_id"], name: "index_members_on_society_id", using: :btree
   end
 
