eb535b609c0733bc25428b13e8788d92430eebfb
sampatbadhe <sampat.badhe@kiprosh.com>
2016-08-28 05:23:32 +0530

    permit update for other attribute of member while signup

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Members::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
